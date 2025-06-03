c148406ef2661b7a9328ae9171a419ee28e232be
leadvictory <intelliresponse911@gmail.com>
2025-06-03 03:10:16 -0500

    remove apt

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "=== Installing Chrome ==="
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb || apt -f install -y
-rm google-chrome-stable_current_amd64.deb
+echo "=== Downloading Portable Chrome ==="
+mkdir -p chrome
+cd chrome
+wget https://storage.googleapis.com/chrome-for-testing-public/120.0.6099.71/linux64/chrome-linux64.zip
+unzip chrome-linux64.zip
+cd ..
 
-echo "=== Installing Python Requirements ==="
+echo "=== Installing Python Dependencies ==="
 pip install -r requirements.txt
